4469747da296e4baf38c6874e7eda857b7ec4f50
PasukkaBig <o_kristina2000@mail.ru>
2024-04-17 21:39:59 +0700

    2 variants of strategies

--- run_detector.py
+++ run_detector.py
@@ -1,10 +1,14 @@
-from emotions_detector.emotions_detector import EmotionsDetector, ChainOfThoughts
+from emotions_detector.emotions_detector import EmotionsDetector
+from emotions_detector.chain_of_thought import ChainOfThoughts
+from emotions_detector.tree_of_thought import TreeOfThoughts
+
 
 def main():
     file_name = "data/data.csv"
     ed = EmotionsDetector(file_name)
-    ed.setStrategy(ChainOfThoughts())
-    ed(1)
+    # ed.setStrategy(ChainOfThoughts())
+    ed.setStrategy(TreeOfThoughts())
+    ed(number=5, shuffle=False)
 
 
 if __name__ == '__main__':
